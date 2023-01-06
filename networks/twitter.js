ff854d69b2da5150527bd0d7e30e0587b276d4e9
Troctom <121873692+Troctom@users.noreply.github.com>
2023-01-06 15:21:42 +0400

    Update twitter.js

--- networks/twitter.js
+++ networks/twitter.js
@@ -22,7 +22,7 @@ function do_twitter_like() {
 function do_twitter_follow() {
 
     state = _STATE_WAIT_TO_CLOSE;
-    wait_time = generateRandom(5, 6);
+    wait_time = generateRandom(5, 5);
 
     var div = document.querySelector('div[data-testid="confirmationSheetConfirm"]');
     if (!div) {
